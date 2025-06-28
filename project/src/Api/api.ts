c3e7e93aed55b34f2891155dcbee757a627e9d9b
sagarpatil2232004 <131273524+sagarpatil2232004@users.noreply.github.com>
2025-06-29 02:42:41 +0530

    added render link as base url

--- project/src/Api/api.ts
+++ project/src/Api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:3000'});
+const API = axios.create({ baseURL: 'https://finance-app-buib.onrender.com'});
 
 const token = localStorage.getItem("token");
 
